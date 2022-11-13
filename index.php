7b34257a6b03c4c4ac216298d79316b9ca3a1d89
anjamodrinic <am20190184@student.fon.bg.ac.rs>
2022-11-13 18:04:19 +0100

    Napravljena pocetna strana

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Postani skijaš</title>
+    <link rel="icon" href="../SkiPhp/img/naslovna.png">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans|Candal|Alegreya+Sans">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/imagehover.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/slike.css">
+
+
+  </head>
+  <body>
+    <?php include 'header.php'; ?>
+
+
+    <section id ="feature" class="section-padding">
+      <div class="container">
+        <div class="row">
+          <div class="header-section text-center">
+           
+            <div class="col-md-12 text-center">
+              <img src="img/animacija.jpg" class="img img-square"> <br><br>
+            </div>
+           <p>Kupovina ski opreme predstavlja ozbiljan zadatak kojem bi trebalo posvetiti potpunu pažnju. Pre same kupovine, vrlo je važno 
+             da odredite vrstu skija koja vama odgovara, koja vam je samim tim i potrebna. Skije se razlikuju u odnosu na vrstu terena za 
+             koje su namenjene, i u odnosu na stil kretanja kojem pripadate. Neki od osnovnih kriterijuma na osnovu kojih se vrši odabir 
+             skija je stepen spremnosti, odnosno iskustva u vožnji. Pored toga vaša visina i težina su isto tako važne informacije koje bi 
+             trebalo da uzmete u obzir. <br> <br>
+             Imajući u vidu koliko je skupo za kupovanje glanc nove opreme za skijanje, želeli smo da kreiramo platformu gde možete kupiti 
+             polovnu opremu u odličnom  stanju. Ukoliko ste penzionisani skijaš i ne znate gde ćete sa svojom opremom, ovde je možete priložiti 
+             i prodati brže i bolje nego što se mogli da zamislite!
+</p>
+<hr class="bottom-line">
+  
+  <div class="col-md-12 text-center">
+    <div class="row">
+        <div class="column">
+            <img src="img/skijas.jpg"  style="width:100%">
+        </div>
+        <div class="column">
+            <img src="img/pada.jpg"  style="width:100%">
+        </div>
+        <div class="column">
+            <img src="img/deca.jpg"  style="width:100%">
+        </div>
+        <div class="column">
+            <img src="img/usnegu.jpg"  style="width:100%">
+        </div>
+    </div>
+    
+  </div>
+  
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <?php include 'footer.php'; ?>
+
+  </body>
+</html>
